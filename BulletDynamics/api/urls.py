c138f27a904505a72dc45330e17611e598c64b21
dimitri.kohler <dimitri.kohler@he-arc.ch>
2021-03-15 15:23:29 +0100

    setup to accept seeds #UPDATE OF DEPENDENCIES
    install of django-seed

--- BulletDynamics/api/urls.py
+++ BulletDynamics/api/urls.py
@@ -4,13 +4,13 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'ammos', views.AmmoViewSet)
-router.register(r'platforms', views.PlatformViewSet)
-router.register(r'cannons', views.CannonViewSet)
-router.register(r'configs', views.ConfigViewSet)
-router.register(r'params', views.ParamViewSet)
-router.register(r'users', views.UserViewSet)
-router.register(r'init_speeds', views.InitSpeedViewSet)
+router.register(r'ammo', views.AmmoViewSet)
+router.register(r'platform', views.PlatformViewSet)
+router.register(r'cannon', views.CannonViewSet)
+router.register(r'config', views.ConfigViewSet)
+router.register(r'param', views.ParamViewSet)
+router.register(r'user', views.UserViewSet)
+router.register(r'init_speed', views.InitSpeedViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
