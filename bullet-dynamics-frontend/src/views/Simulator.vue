7d4c2b859cb3f92b70324c7bfa99599ceec82f4a
dimitri.kohler <dimitri.kohler@he-arc.ch>
2021-03-26 09:00:12 +0100

    Creation of simulator page

--- bullet-dynamics-frontend/src/views/Simulator.vue
+++ bullet-dynamics-frontend/src/views/Simulator.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SimulatorConfigElement/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import SimulatorConfigElement from '@/components/SimulatorConfigElement.vue'
 
 export default {
-  name: 'Home',
+  name: 'Simulator',
   components: {
-    HelloWorld
+    SimulatorConfigElement
   }
 }
 </script>
