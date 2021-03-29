c819d9aaaddb1727b43b47205c9219b13e6397c7
dimitri.kohler <dimitri.kohler@he-arc.ch>
2021-03-29 15:46:45 +0200

    Implementation of google charts on simulator page

--- bullet-dynamics-frontend/src/views/Simulator.vue
+++ bullet-dynamics-frontend/src/views/Simulator.vue
@@ -1,20 +1,47 @@
 <template>
   <div>
-    <SimulatorConfigList/> 
-    <SimulatorParamList/>  
+    <v-container>
+      <v-row justify="space-between">
+        <v-col cols="4">
+          <v-container>
+            <v-row justify="space-between">
+              <v-col>
+                <SimulatorConfigList />
+              </v-col>
+            </v-row>
+            <v-row justify="space-between">
+              <v-col>
+                <SimulatorParamList />
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-col>
+        <v-col cols="8">
+          <v-container>
+            <v-row justify="space-between">
+              <v-col>
+                <GraphComponent />
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import SimulatorConfigList from '../components/SimulatorConfigList.vue'
-import SimulatorParamList from '../components/SimulatorParamList.vue'
+import GraphComponent from "../components/GraphComponent.vue";
+import SimulatorConfigList from "../components/SimulatorConfigList.vue";
+import SimulatorParamList from "../components/SimulatorParamList.vue";
 
 export default {
-  name: 'Simulator',
+  name: "Simulator",
   components: {
     SimulatorConfigList,
-    SimulatorParamList
-  }
-}
+    SimulatorParamList,
+    GraphComponent,
+  },
+};
 </script>
