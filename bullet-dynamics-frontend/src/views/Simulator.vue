40ffa3dde265b1e30a14fa1338dff0406de8192e
dimitri.kohler <dimitri.kohler@he-arc.ch>
2021-04-08 17:21:22 +0200

    tests with axios implementation

--- bullet-dynamics-frontend/src/views/Simulator.vue
+++ bullet-dynamics-frontend/src/views/Simulator.vue
@@ -43,13 +43,48 @@
 import GraphComponent from "../components/GraphComponent.vue";
 import SimulatorConfigList from "../components/SimulatorConfigList.vue";
 import SimulatorParamList from "../components/SimulatorParamList.vue";
+import { getAPI } from '../plugins/axios-api';
 
 export default {
   name: "Simulator",
+  data () {
+    return {
+        configs: []
+      }
+  },
   components: {
     SimulatorConfigList,
     SimulatorParamList,
     GraphComponent,
   },
+  methods: {
+    loadConfigs() {
+      let _this = this;
+
+      // Get loot history
+      this.configs = [];
+      this.loadingConfigs = true;
+      
+      getAPI
+        .get("/configs/")
+        .then(response => {
+          console.log('Getting configs');
+          _this.configs = response.data;
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        })
+        .then(() => {
+          _this.loadingConfigs = false;
+        });
+    },
+  },
+  created: function () {
+    let _this = this;
+
+    _this.loadConfigs();
+  }
+
 };
 </script>
