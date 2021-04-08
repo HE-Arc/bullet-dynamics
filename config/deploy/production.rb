ada3c3d7ea7f5a9657736cc80c8a594e9f8ee4c3
ma2s <massimo.desantis>
2021-04-08 01:15:41 -0700

    pruduction config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -10,63 +10,6 @@
 server "bullet.srvz-webapp.he-arc.ch", user: "poweruser", roles: %w{app db web}, port: 1444
 set :deploy_to, "/var/www/#{fetch(:application)}"
 
-namespace :django do
-
-    def venv_path
-        File.join(shared_path, 'env')
-    end
-
-    desc 'Apply migrations'
-    task :migrate do
-        on roles(:web) do |h|
-            execute "cp #{shared_path}/.env #{release_path}/BulletDynamics/BulletDynamics/.env"
-            execute "source #{venv_path}/bin/activate && python3 #{release_path}/BulletDynamics/manage.py migrate && cd #{release_path}/BulletDynamics &&python3 #{release_path}/BulletDynamics/manage.py collectstatic --noinput"
-	    end
-    end
-    
-end
-
-after 'django:migrate', 'uwsgi:restart'
-
-namespace :uwsgi do
-    desc 'Restart application'
-    task :restart do
-        on roles(:web) do |h|
-	        execute :sudo, 'sv reload uwsgi'
-	    end
-    end
-end
-
-after 'deploy:updating', 'python:update_venv'
-
-namespace :python do
-
-    def venv_path
-        File.join(shared_path, 'env')
-    end
-
-    desc 'update venv'
-    task :update_venv do
-        on roles([:app, :web]) do |h|
-            execute "source #{venv_path}/bin/activate && #{venv_path}/bin/pip install -r #{release_path}/requirements.txt"
-        end
-    end
-end
-
-
-after 'deploy:publishing', 'frontend:compile'
-
-namespace :frontend do
-    desc 'compile frontend'
-    task :compile do
-        on roles(:web) do |h|
-	        execute "cd #{release_path}/bullet-dynamics-frontend && npm install && npm run build"
-	    end
-    end
-end
-
-
-
 # role-based syntax
 # ==================
 
