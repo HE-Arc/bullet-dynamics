83e998ca88ba8008a1edfa07fecec1728df0db7f
dimitri.kohler <dimitri.kohler@he-arc.ch>
2021-04-16 10:47:47 +0200

    Migration to new mysql db

--- BulletDynamics/BulletDynamics/settings.py
+++ BulletDynamics/BulletDynamics/settings.py
@@ -92,8 +92,12 @@ WSGI_APPLICATION = 'BulletDynamics.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'bullet',
+        'USER': 'root',
+        'PASSWORD': 'toor',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
