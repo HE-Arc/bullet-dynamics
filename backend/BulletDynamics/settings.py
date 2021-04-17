a9aa70426ec1756aa782ed7f7de93410cdf5b852
ma2s <massimo.desantis>
2021-04-17 04:21:21 -0700

    CORS fail correction ?

--- backend/BulletDynamics/settings.py
+++ backend/BulletDynamics/settings.py
@@ -61,10 +61,8 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ORIGIN_WHITELIST = (
-    'http://localhost:8080',
-    'http://127.0.0.1:8080',
-)
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'BulletDynamics.urls'
 
