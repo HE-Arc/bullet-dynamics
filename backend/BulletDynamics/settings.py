ec89cf9c13e5393bc4ee8bb8160b3f2440e80f4b
ma2s <massimo.desantis>
2021-04-17 03:58:58 -0700

    static root added

--- backend/BulletDynamics/settings.py
+++ backend/BulletDynamics/settings.py
@@ -140,6 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = "static/"
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
