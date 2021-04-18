6271df5ce30e84442992e50726d329215e0dc9d5
dimitri.kohler <dimitri.kohler@he-arc.ch>
2021-04-18 21:43:06 +0200

    change request url from local to server

--- frontend/src/plugins/axios-api.js
+++ frontend/src/plugins/axios-api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const getAPI = axios.create({
-    baseURL: 'https://bullet.srvz-webapp.he-arc.ch/backend/', // need to change it later (use a env) (but not enough time to do so)
-    //baseURL: 'http://localhost:8000/backend/',
+    //baseURL: 'https://bullet.srvz-webapp.he-arc.ch/backend/', // need to change it later (use a env) (but not enough time to do so)
+    baseURL: 'http://localhost:8000/backend/',
     timeout: 5000,
 })
 
